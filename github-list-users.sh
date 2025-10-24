327f8fdaca7be8395e7795ab163f59e8d42c55a9
Venkatesh P R <venky@Venkatesh.local>
2025-10-24 16:00:12 +0530

    Added Script File

---
+++ github-list-users.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+######################################
+# Author: Venkatesh
+# Date: 14th Oct 2025
+# About: This Script will give you list of GitHub Collaborators
+# Version: v.01
+######################################
+
+API_URL="https://api.github.com"
+
+USERNAME=$username
+TOKEN=$token
+
+REPO_OWNER=$1
+REPO_NAME=$2
+function get_function {
+	local endpoint="$1"
+	curl -s -u "${USERNAME}:${TOKEN}" "${API_URL}/${endpoint}"
+}
+function list_of_collaborators {
+	local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+	collaborators="$(get_function "$endpoint" | jq -r '.[] | select(.permissions.admin == true) | .login ')"
+	if [[ -z "$collaborators" ]]; then
+		echo "No users for ${REPO_OWNER}/${REPO_NAME}"
+	else
+		echo "List of Users for this ${REPO_OWNER}/${REPO_NAME} Repository"
+		echo "$collaborators"
+	fi
+}
+list_of_collaborators
